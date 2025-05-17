62c00bcbc0e0693f248752e4fbff5518bb23963d
Jusepepe <saltamontehz@gmail.com>
2025-05-17 15:21:11 -0500

    Uniéndosé a la rama principal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,5 +3,8 @@
     <!-- Top nav-->
     <x-header></x-header>
     <!--aside-menu>-->
-    <x-aside-menu></x-aside-menu>
+    <div class="flex min-h-0 flex-1 overflow-hidden">
+        <x-aside-menu></x-aside-menu>
+        <x-image-plot></x-image-plot>
+    </div>
 </x-app-layout>
